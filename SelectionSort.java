57eeb73485941ff25bf495833ec5923c8de85b97
Vaishnavi Gupta <122524433+VaishnavixGupta@users.noreply.github.com>
2024-07-17 14:41:50 +0530

    SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,11 +1,6 @@
-package com.vaish.Sorting.SelectionSort;
-
 import java.util.Arrays;
 
 public class SelectionSort {
-    //Time Complexity :
-    //                   Best: O(N^2)
-    //                   Worst: O(N^2)     //n is size of array
     public static void main(String[] args) {
         int[] arr={4,5,1,2,3};
         selectionSort(arr);
